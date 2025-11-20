81597b29698f68e32a4bf7a5b52c2edbc4625c5e
ryo_ta_na <ryoutana@icloud.com>
2025-11-21 00:24:14 +0800

    Introduced Configmap

--- scripts/deploy_local.sh
+++ scripts/deploy_local.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 kubectl apply -f ./k8s/namespace.yaml
-kubectl apply -f ./k8s/db-secret.yaml
+kubectl apply -f ./k8s/configmap.yaml
+kubectl apply -f ./k8s/secret.yaml
 kubectl apply -f ./k8s/backend-deployment.yaml
 kubectl apply -f ./k8s/frontend-deployment.yaml
 kubectl apply -f ./k8s/hpa.yaml
